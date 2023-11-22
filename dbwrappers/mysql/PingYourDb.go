9b9a63f98f311b062964ee439a37e2de74abb584
DeexithParand2k2 <deexith2002@gmail.com>
2023-11-22 12:06:05 +0530

    finalized base setup for our module

--- dbwrappers/mysql/PingYourDb.go
+++ dbwrappers/mysql/PingYourDb.go
@@ -1,36 +1,17 @@
 package mysqlutility
 
-import (
-	"log"
-	"github.com/gin-gonic/gin"
-	"net/http"
-)
-
-// @ query key : "databasename"
-func PingYourDb(c *gin.Context){
-
-	queryDbName := c.Query("databasename")
-
-	if queryDbName==""{
-		log.Fatal("Error: Query doesn't contain any database name")
-		c.JSON(http.StatusBadRequest, gin.H{"Query Error": "Query doesn't contain any database name"})
-		return
-	}
-	
-	db, err := OpenDbConnect(queryDbName)
+// @params 1 databasename string
+// @return (string,error)
+func PingYourDb(databaseName string) (string, error) {
+	db, err := OpenDbConnect(databaseName) // returns
 	if err != nil {
-		log.Print("Error: error on connection to db")
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return 
+		return "", err
 	}
-	defer db.Close()
+	defer db.Close() // after pinging close it
 
-	if err = db.Ping(); err != nil{
-		log.Print("Error: Pinging error to db")
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return	
+	if err = db.Ping(); err != nil {
+		return "", err
 	}
- 
-	c.IndentedJSON(http.StatusCreated, gin.H{"Success":"Connection to DB "+queryDbName+" successfull"})
 
+	return "Success pinging db" + databaseName, nil
 }
--- main.go
+++ main.go
@@ -1,18 +1,32 @@
-package main 
+package main
 
 import (
+	"net/http"
+
+	mysqlutility "github.com/DeexithParand2k2/CrudifyGO/dbwrappers/mysql"
 	"github.com/gin-gonic/gin"
-	"github.com/DeexithParand2k2/CrudifyGO/dbwrappers/mysql"
 )
 
-func main(){
+// testing our pingdb func
+func testPingYourDb(c *gin.Context) {
+
+	databaseNameQuery := c.Query("databasename")
+	status, err := mysqlutility.PingYourDb(databaseNameQuery)
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"Error": err.Error()})
+		return
+	}
+
+	c.IndentedJSON(http.StatusAccepted, gin.H{"Message": status})
+}
+
+func main() {
 	router := gin.Default()
 
-	router.GET("/pingdb",mysqlutility.PingYourDb) // ping db with query as db name
+	router.GET("/pingdb", testPingYourDb) // ping db with query as db name
 	//router.GET("/createdb",CreateYourDb) // create a db and ping it
 	//router.GET("/gettablesdb",GetTablesDb) // get tables in a db
 	//router.GET("/gettableschema",GetTableSchema) // get schema of a table
 
 	router.Run("localhost:8000")
 }
-
