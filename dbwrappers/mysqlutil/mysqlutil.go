bbc64a1bcb5176dbd2dff339bd3db18367485080
DeexithParand2k2 <deexith2002@gmail.com>
2023-12-10 23:26:42 +0530

    createdb restructured

--- dbwrappers/mysqlutil/mysqlutil.go
+++ dbwrappers/mysqlutil/mysqlutil.go
@@ -1,7 +1,14 @@
 package mysqlutil
 
-import "github.com/DeexithParand2k2/CrudifyGO/dbwrappers/mysqlutil/table"
+import (
+	"github.com/DeexithParand2k2/CrudifyGO/dbwrappers/mysqlutil/db"
+	"github.com/DeexithParand2k2/CrudifyGO/dbwrappers/mysqlutil/table"
+)
 
 func ListTablesDb(databasename string) ([]string, error) {
 	return table.ListTablesDb(databasename)
+}
+
+func CreateDb(databasename string) error {
+	return db.CreateDb(databasename)
 }
